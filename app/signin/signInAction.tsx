7f3ad2efd66da2dc412693036a26608ecbccb351
Bolaji Adedeji <badedeji1@gmail.com>
2025-02-05 13:28:03 +0000

    serverUrlChange4

--- app/signin/signInAction.tsx
+++ app/signin/signInAction.tsx
@@ -8,7 +8,7 @@ export async function signInAction (prevState:string, formData: FormData): Promi
   const password = formData.get("password");
  
 
-  const res = await fetch(`${process.env.VERCEL_API}/api/signin`, {
+  const res = await fetch(`https://mealplanner-umber.vercel.app/api/signin`, {
     method: "POST",
     headers: {"Content-Type":"application/json"},
     body: JSON.stringify({ email, password})
